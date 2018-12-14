6f6c06629298f57026e302bb22bad73b45218dbb
Mariia Anisimova <just-another-log-in@yandex.ru>
2018-12-14 15:40:25 +0300

    add UserService

--- src/main/java/se/ifmo/ru/service/UserService.java
+++ src/main/java/se/ifmo/ru/service/UserService.java
@@ -1,7 +1,15 @@
 package se.ifmo.ru.service;
 
 import org.springframework.stereotype.Service;
+import se.ifmo.ru.domain.model.User;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface UserService {
+
+    Optional<User> loadById(Long id);
+
+    Optional<List<User>> getAllPoints();
 }
--- src/main/java/se/ifmo/ru/service/UserServiceImlp.java
+++ src/main/java/se/ifmo/ru/service/UserServiceImlp.java
@@ -1,7 +1,31 @@
 package se.ifmo.ru.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.ifmo.ru.domain.dao.UserRepository;
+import se.ifmo.ru.domain.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImlp implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImlp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> loadById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<List<User>> getAllPoints() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users :: add);
+        return Optional.of(users);
+    }
 }

8373c844dd3719327bd25cb58f8e56978b504149
Mariia Anisimova <just-another-log-in@yandex.ru>
2018-12-14 13:17:47 +0300

    initial commit

---
+++ src/main/java/se/ifmo/ru/IadApplication.java
@@ -0,0 +1,14 @@
+package se.ifmo.ru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IadApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/se/ifmo/ru/IadApplicationTests.java
@@ -0,0 +1,17 @@
+package se.ifmo.ru;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

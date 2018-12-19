f24b1dca3820fd30aef12abd7f64f0ee0ffa3ba7
Mariia Anisimova <just-another-log-in@yandex.ru>
2018-12-19 11:21:14 +0300

    add index.html, properties, configs

---
+++ src/main/js/app.js
@@ -0,0 +1,23 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const client = require('./client');
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {points: []};
+    }
+
+    componentDidMount() {
+        client({method: 'GET', path: '/api/points'}).done(response => {
+            this.setState({points: response.entity._embedded.points});
+    });
+    }
+
+    render() {
+        return (
+            <PointsList points={this.state.points}/>
+    )
+    }
+}

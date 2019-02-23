e09deffed92905627612a8d7d088c057345156da
seductorAmadeus <seductorAmadeus@gmail.com>
2019-02-23 19:29:23 +0300

    add form login auth

--- src/main/java/se/ifmo/ru/config/SecurityConfig.java
+++ src/main/java/se/ifmo/ru/config/SecurityConfig.java
@@ -7,7 +7,15 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.stereotype.Component;
 import se.ifmo.ru.service.UserDetailsService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -24,9 +32,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().fullyAuthenticated();
-        http.httpBasic();
-        http.csrf().disable();
+//        http.authorizeRequests().anyRequest().fullyAuthenticated();
+//        http.httpBasic();
+        http
+                .csrf().disable()
+                .exceptionHandling()
+//                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin()
+//                .successHandler(mySuccessHandler)
+//                .failureHandler(myFailureHandler)
+                .and()
+                .logout();
     }
 
+    @Component
+    public final class RestAuthenticationEntryPoint
+            implements AuthenticationEntryPoint {
+
+        @Override
+        public void commence(
+                final HttpServletRequest request,
+                final HttpServletResponse response,
+                final AuthenticationException authException) throws IOException {
+
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                    "Unauthorized");
+        }
+    }
 }
--- src/main/java/se/ifmo/ru/controller/RegistrationController.java
+++ src/main/java/se/ifmo/ru/controller/RegistrationController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +16,8 @@ import se.ifmo.ru.domain.model.User;
 import se.ifmo.ru.service.PointService;
 import se.ifmo.ru.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -30,11 +35,6 @@ public class RegistrationController {
     @PreAuthorize("hasRole('ROLE_OTHER')")
     @RequestMapping("/login")
     public ResponseEntity<String> login() { return new ResponseEntity<>("login", HttpStatus.OK); }
-
-    @RequestMapping("/logout")
-    public String logout() {
-        return "index";
-    }
 
     @RequestMapping("/registration")
     public ResponseEntity<String> registration() {return new ResponseEntity<>("registration", HttpStatus.OK); }

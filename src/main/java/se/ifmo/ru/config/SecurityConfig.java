16beadb265a5a3052697f580ed9a3c368c2ba924
mishkamashka <anisimova.mariia.mm@gmail.com>
2019-02-23 15:44:21 +0300

    add method in securityConfig

--- src/main/java/se/ifmo/ru/config/SecurityConfig.java
+++ src/main/java/se/ifmo/ru/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package se.ifmo.ru.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import se.ifmo.ru.service.UserDetailsService;
 
 @Configuration
@@ -27,6 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().fullyAuthenticated();
         http.httpBasic();
         http.csrf().disable();
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(16);
     }
 
 }
